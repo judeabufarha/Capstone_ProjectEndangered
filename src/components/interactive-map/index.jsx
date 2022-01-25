f9c97e8cf05570969ce88a5c04e8f8bc5ff9969b
CarlyS2240 <carly.strathearn@carleton.ca>
2022-01-25 10:51:09 -0500

    Added custom marks and tooltips

--- src/components/interactive-map/index.jsx
+++ src/components/interactive-map/index.jsx
@@ -10,7 +10,7 @@ import {
   } from "react-simple-maps";
 
 import "./styles.css";
-import { scaleDown } from "react-burger-menu";
+import { MapMarker } from "../map-marker";
 
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
@@ -18,6 +18,7 @@ const geoUrl =
 export const Map = (props) => {
 
     return (
+        <>
         <div className="flex-map">
             <ComposableMap projectionConfig={{}}>
             <Geographies geography={geoUrl}>
@@ -35,54 +36,16 @@ export const Map = (props) => {
                 ))
                 }
             </Geographies>
-            <Marker coordinates={[-74.006, 40.7128]} style={{
-         default: {
-            fill: "#72C2E4",
-            stroke: "#94D2ED",
-            strokeWidth: 24,
-            strokeOpacity: 0.4,
-            outline: "none",
-            strokeDashoffset: 100,
-            transition: "ease-in-out",
-         },
-         hover: {
-            fill:"#c8e0c8",
-            fillOpacity:0.6,
-            stroke: "#2D6C73",
-            strokeWidth: 1.5,
-            outline: "none",
-         },
-         
-      }}>
-                <circle r={12}/>
-               
-            </Marker>
-            <Marker coordinates={[20, 10]} style={{
-         default: {
-            fill: "#72C2E4",
-            stroke: "#94D2ED",
-            strokeWidth: 18,
-            strokeOpacity: 0.4,
-            outline: "none",
-            strokeDashoffset: 100,
-            transition: "ease-in-out",
-         },
-         hover: {
-            //transform: `scale(1.5, 1.5)`,
-            origin: {x: 'center', y:'center'},
-            fill:"#c8e0c8",
-            fillOpacity:0.6,
-            stroke: "#2D6C73",
-            strokeWidth: 1.5,
-            outline: "none",
-         },
-         
-      }}>
-                <circle r={9}/>
-               
-            </Marker>
             </ComposableMap>
+            <div className="map-marker-01-container">
+                <MapMarker className="map-marker-blue" text="Canada 5000-7000"></MapMarker>
+            </div>
+            <div className="map-marker-02-container">
+                <MapMarker className="map-marker-blue-02" text="Southern Arctic"></MapMarker>
+            </div>
         </div>
+        </>
+        
     );
 }
 
---
+++ src/components/map-marker/index.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import "./styles.css";
+import { useState } from "react";
+import { useEffect} from "react";
+import Tooltip from "react-simple-tooltip"
+import {css} from "styled-components"
+import { MapTooltip } from "../map-tooltip";
+
+export const MapMarker = (props) => {
+
+    return (
+        <div className="centered">
+            <Tooltip 
+                customCss={css` top: 35px; white-space: pre-line;
+                `} content={props.text} background="#c4e1e4" border="none" placement="right" padding={20} offset={70} radius={8} color="#013440" arrow={15}
+            >
+                <div style={{top: "-30px"}} className="circle"></div>
+            </Tooltip>
+                
+            
+        </div>
+    );
+}
+
+MapMarker.propTypes = {
+    text: PropTypes.string.isRequired,
+}
